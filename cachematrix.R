e0191d891c95c63df72b12341f52aa9942bc979e
GerardoNevarez <gnevarez@gmail.com>
2015-04-24 22:51:55 -0400

    Initial implementation of caching-enabling functions, includes skeleton function to detect changes in inversed matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,50 @@
 ## This functions help cache the result of obtaiing the inverse of a matrix. Is
 ## expected thay any matix received as parameter is a square matrix than can be
-## reversed. 
+## reversed.
+
+## Based on the MakeVector and cacheMean functions in the instructions for
+## the programming assignment 2 (Coursera course "R Programming", by Roger D.
+## Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD; John Hopkins Bloomberg School
+## of Public Health)
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    ## Variable to store the cached result
+    cache <- NULL
+    
+    ## function to set a new value for the matrix (deletes the cached result)
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+    
+    ## Retrieves the matrix to be "inversed"
+    get <- function() x
+    
+    ## Sets the cache to the given input (called from cacheSolve)
+    setCachedInverse <- function(inverse) cache <<- inverse
+    
+    ## Retrieves the cached result
+    getCachedInverse <- function() cache
+    
+    ## Determines if the matrix from where the inverse was calculated has
+    ## changed. 
+    hasChanged <- function hasChanged(){
+        ##If the is no cache the response is FALSE since the is nothing to
+        ## compare to.
+        if (NULL == cache){
+            return TRUE
+        }
+       
+        ## TODO Finish implementation
+        TRUE
+    }
+    list(set = set, get = get,
+         setmean = setCachedInverse,
+         getmean = getCachedInverse,
+         hasChanged = hasChanged)
 
 }
 
@@ -21,4 +61,5 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         ## X is the result of a call to makeCacheMatrix()
+
 }
